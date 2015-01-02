20674e56a5e873117195330ca3e3341c1502ff1f
Bo Lopker <lopkerk@gmail.com>
2015-01-02 11:48:55 -0800

    fix file name and have more robust json/file checking.

--- tests/themes-exist.spec.js
+++ tests/themes-exist.spec.js
@@ -1,7 +1,25 @@
 'use strict';
 
+// This could probably be more efficient.
 describe('All the themes exist', function() {
-	it('Any theme has an entry in the themes.json file and vice versa', function() {
-		expect(themesJSON.length).to.be.equal(themesFiles.length);
+
+    var fileNames = themesFiles.map(function(fileStat) {
+        return fileStat.name;
+    }).sort();
+
+    var jsonNames = themesJSON.map(function(theme) {
+        return theme.FileName;
+    }).sort();
+
+	it('All theme files exist in themes JSON', function() {
+        fileNames.forEach(function(fileName, i) {
+            expect(fileName).to.equal(jsonNames[i]);
+        });
 	});
+
+    it('All theme JSON entries exist in thems folder', function() {
+        jsonNames.forEach(function(jsonName, i) {
+            expect(jsonName).to.equal(fileNames[i]);
+        });
+    });
 });
