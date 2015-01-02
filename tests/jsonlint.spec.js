815340209d02fa536e63c281182cdf6de3e13e42
Bo Lopker <lopkerk@gmail.com>
2015-01-02 12:04:29 -0800

    tabs

--- tests/jsonlint.spec.js
+++ tests/jsonlint.spec.js
@@ -3,16 +3,16 @@ var fs = require('fs');
 var jsonlint = require('jsonlint');
 
 describe('Check the themes.json file', function() {
-    it('The themes.json is a valid json file', function() {
-        var jsonSting = fs.readFileSync('./themes.json', 'utf8');
-        expect(jsonlint.parse(jsonSting)).to.be.an('array');
-    });
+	it('The themes.json is a valid json file', function() {
+		var jsonSting = fs.readFileSync('./themes.json', 'utf8');
+		expect(jsonlint.parse(jsonSting)).to.be.an('array');
+	});
 });
 
 describe('Check the themes.json entries are valid', function() {
-    it('Title should not have surrounding whitespace', function() {
-        themesJSON.forEach(function(theme) {
-            expect(theme.Title.trim()).to.equal(theme.Title);
-        });
-    });
+	it('Title should not have surrounding whitespace', function() {
+		themesJSON.forEach(function(theme) {
+			expect(theme.Title.trim()).to.equal(theme.Title);
+		});
+	});
 });
--- tests/themes-exist.spec.js
+++ tests/themes-exist.spec.js
@@ -3,23 +3,23 @@
 // This could probably be more efficient.
 describe('All the themes exist', function() {
 
-    var fileNames = themesFiles.map(function(fileStat) {
-        return fileStat.name;
-    }).sort();
+	var fileNames = themesFiles.map(function(fileStat) {
+		return fileStat.name;
+	}).sort();
 
-    var jsonNames = themesJSON.map(function(theme) {
-        return theme.FileName;
-    }).sort();
+	var jsonNames = themesJSON.map(function(theme) {
+		return theme.FileName;
+	}).sort();
 
 	it('All theme files exist in themes JSON', function() {
-        fileNames.forEach(function(fileName, i) {
-            expect(fileName).to.equal(jsonNames[i]);
-        });
+		fileNames.forEach(function(fileName, i) {
+			expect(fileName).to.equal(jsonNames[i]);
+		});
 	});
 
-    it('All theme JSON entries exist in thems folder', function() {
-        jsonNames.forEach(function(jsonName, i) {
-            expect(jsonName).to.equal(fileNames[i]);
-        });
-    });
+	it('All theme JSON entries exist in thems folder', function() {
+		jsonNames.forEach(function(jsonName, i) {
+			expect(jsonName).to.equal(fileNames[i]);
+		});
+	});
 });
